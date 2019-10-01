b3cc86a46770cf70b0b34f846cc1d778cf81de87
zhenyatos <zhenyatos999@gmail.com>
2019-10-02 00:54:34 +0300

    Add recolor filter

--- main.cpp
+++ main.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
 #include "png_toolkit.h"
+#include "filters.h"
 
-int main( int argc, char *argv[] )
-{
+int main (int argc, char *argv[]) {
     // toolkit filter_name base_pic_name sudent_tool student_pic_name limitPix limitMSE
     // toolkit near test images!
-    try
-    {
+    try {
         if (argc != 3)
             throw "Not enough arguments";
 
         png_toolkit studTool;
         studTool.load(argv[1]);
+		recolorHalfImage(studTool, 0xFF);
         studTool.save(argv[2]);
 
-    }
-    catch (const char *str)
-    {
+    } catch (const char *str) {
         std::cout << "Error: " << str << std::endl;
         return 1;
     }
